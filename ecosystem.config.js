6ce2f9463735a24208a18402381b8e14b952c416
Krushna Patil <krushnapatil2025@gmail.com>
2026-07-26 18:46:09 +0530

    fix: switch PM2 to fork mode to fix EADDRINUSE port 3000 conflict

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,8 @@ module.exports = {
       script: "node_modules/.bin/next",
       args: "start",
       cwd: "./",
-      instances: "max",           // Use all CPU cores (cluster mode)
-      exec_mode: "cluster",
+      instances: 1,
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
